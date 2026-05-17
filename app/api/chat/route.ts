5f243376c4e0327664d98de2cb720202c9cd7b4e
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2026-05-17 14:59:04 -0400

    patch

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: NextRequest) {
       body: JSON.stringify({
         messages,
         stream: true,
-        temperature: 0.2,
+        temperature: 0.1,
         top_p: 0.8,
         repetition_penalty: 1.1,
         extra_body: { top_k: 20 },
